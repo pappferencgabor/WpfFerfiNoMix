7ed40e54aec2ab13c1d2df3510848aac1a6d8871
pappferencgabor <70635774+pappferencgabor@users.noreply.github.com>
2023-11-30 11:08:37 +0100

    feltöltés, mixelés nem mindig működik

--- FerfiNoMix/MainWindow.xaml.cs
+++ FerfiNoMix/MainWindow.xaml.cs
@@ -24,10 +24,41 @@ namespace FerfiNoMix
         {
             InitializeComponent();
         }
+        Random vel = new Random();
 
         private void txtNo_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Enter)
+            {
+                lbNok.Items.Add(txtNo.Text);
+                txtNo.Text = "";
+            }
+        }
 
+        private void txtFerfi_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                lbFerfiak.Items.Add(txtFerfi.Text);
+                txtFerfi.Text = "";
+            }
+        }
+
+        private void btnMix_Click(object sender, RoutedEventArgs e)
+        {
+            int parok = 0;
+            if (lbFerfiak.Items.Count > lbNok.Items.Count) { parok = lbNok.Items.Count; }
+            else { parok = lbFerfiak.Items.Count; }
+
+            for (int i = 0; i < parok; i++)
+            {
+                int Ferfi = vel.Next(0, parok-1);
+                int No = vel.Next(0, parok - 1);
+
+                lbMix.Items.Add($"{lbFerfiak.Items[Ferfi]} + {lbNok.Items[No]}");
+                lbFerfiak.Items.RemoveAt(Ferfi);
+                lbNok.Items.RemoveAt(No);
+            }
         }
     }
 }
